14d586fde36b872a67558bdd18efe27993acf810
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2025-01-07 13:13:48 +0530

    Refactor user state management in App component to retrieve values from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,22 @@ import AppRoutes from './Routes/AppRoutes';
 
 export const UserContext = createContext();
 
+// Function to retrieve user from localStorage
+const getUserFromLocalStorage = () => {
+  const savedUser = localStorage.getItem('user');
+  return savedUser ? JSON.parse(savedUser) : null;
+};
+
+// Function to retrieve isLoggedIn from localStorage
+const getIsLoggedInFromLocalStorage = () => {
+  const savedLoggedIn = localStorage.getItem('isLoggedIn');
+  return savedLoggedIn === 'true';
+};
+
 function App() {
   // Get this from localStorage
-  const [user, setUser] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(getUserFromLocalStorage);
+  const [isLoggedIn, setIsLoggedIn] = useState(getIsLoggedInFromLocalStorage);
 
   return (
     <UserContext.Provider value={{ user, setUser, isLoggedIn, setIsLoggedIn }}>
@@ -16,6 +28,7 @@ function App() {
         <AppRoutes userRole={user?.role?.roleName || ""} isLoggedIn={isLoggedIn} />
       </Router>
     </UserContext.Provider>
+    //
   )
 }
 
